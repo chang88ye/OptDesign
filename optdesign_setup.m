c0c75a56a9263328de755b78b0960616409caaf7
SHOUYONG JIANG <chang88ye@users.noreply.github.com>
2021-12-07 00:22:19 +0000

    Add files via upload

--- optdesign_setup.m
+++ optdesign_setup.m
@@ -7,6 +7,7 @@ catch
     error(' > cobratoolbox is not available.\n');
 end
 
+warning on
 % use Gurobi (if installed) as the default solver for LP, QP and MILP problems
 solOK=changeCobraSolver('gurobi', 'ALL', 0);
 if ~solOK
@@ -19,6 +20,7 @@ if isempty(str)
     error('Current directory is not NIHBA, please change it to OptDesign.\n')
 end
 
+warning off
 % add OptDesign to matlab search path
 curPath=fileparts(str);
 addpath(genpath(curPath));
